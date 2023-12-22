2e7c8628cddf16e44a3feed3afc51a6ae29162ab
golden-dood <catererzach@yahoo.com>
2023-12-22 12:35:43 -0600

    updated tables for metadata

--- app/components/empty_tables_for_now.py
+++ app/components/empty_tables_for_now.py
@@ -10,20 +10,14 @@ I am not sure what elements we want in the table of the metadata, I am leaving t
 https://dash-bootstrap-components.opensource.faculty.ai/docs/components/table/
 for now 
 """
-table_header = [
-    html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
-]
 
-row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
-row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
-row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
-row4 = html.Tr([html.Td("Trillian"), html.Td("Astra")])
+# Example DataFrame
+rows = list("ABCDEFGH")
+columns = [str(num).zfill(2) for num in range(1, 13)]
+data = [[row + col for col in columns] for row in rows]
+df = pd.DataFrame(data, columns=columns, index=rows)
 
-table_body = [html.Tbody([row1, row2, row3, row4])]
-
-table = dbc.Table(table_header + table_body,
-                    bordered=True,
-                    dark=True,
-                    hover=True,
-                    responsive=True,
-                    striped=True,)
+# Create a dbc.Table from the DataFrame
+table = dbc.Table.from_dataframe(
+    df, striped=True, bordered=True, hover=True, index=True
+)
