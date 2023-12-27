8ee8da4e658f969a1cbeed96c0d8a753974ac9c6
golden-dood <catererzach@yahoo.com>
2023-12-27 09:07:58 -0600

    formatting dash app w/ best practices

--- app/components/meta_data_.py
+++ app/components/meta_data_.py
@@ -5,6 +5,11 @@
 ########################################################################
 import dash_bootstrap_components as dbc
 from dash import dcc, html
+from dash import dash_table
+from dash.dependencies import Input, Output, State
+
+# Importing Components
+from app.utils.create_df_from_user_input import create_empty_df_from_inputs
 
 ########################################################################
 ####                                                                ####
@@ -38,3 +43,97 @@ meta_data = dbc.Container([
     ]),
 ],
     style={"paddingTop": "80px"}) # adjust white space between metadata tab and tabs of metadata content
+
+
+########################################################################
+####                                                                ####
+####                             Callbacks                          ####
+####                                                                ####
+########################################################################
+def update_metadata_tables(app):
+     # Create a callback to update the table based on user inputs
+    @app.callback(
+        [Output("table-container-batch", "children"),
+        Output("table-container-species", "children"),
+        Output("table-container-strains", "children"),
+        Output("table-container-stages", "children"),
+        Output("table-container-treatments", "children"),
+        Output("table-container-conc", "children"),
+        Output("table-container-other", "children")
+        ],
+        [Input("total-num-rows", "value"),
+        Input("total-well-cols", "value")]
+    )
+    def update_table(rows, cols):
+        default_cols = 12
+        default_rows = 8
+        if rows is None:
+            rows = default_rows
+        if cols is None:
+            cols = default_cols
+
+        df_empty = create_empty_df_from_inputs(rows, cols)
+        table_batch = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-batch'
+        )
+        table_species = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-species'
+        )
+        table_stages = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-stages'
+        )
+        table_strains = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-strains'
+        )
+        table_treatment = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-species'
+        )
+        table_conc = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-conc'
+        )
+        table_other = dash_table.DataTable(
+            data=df_empty.reset_index().to_dict('records'),
+            columns=[{'name': 'Row', 'id': 'index', 'editable': False}] +
+            [{'name': col, 'id': col} for col in df_empty.columns],
+            editable=True,
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'center'},
+            id='dynamic-table-container-other'
+        )
+        return table_batch, table_species, table_strains, table_stages, table_treatment, table_conc, table_other
---
+++ app/pages/save_page_content.py
@@ -0,0 +1,154 @@
+########################################################################
+####                                                                ####
+####                             Imports                            ####
+####                                                                ####
+########################################################################
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import yaml
+import os
+
+########################################################################
+####                                                                ####
+####                              Layout                            ####
+####                                                                ####
+########################################################################
+save_page_content = dbc.ModalBody(
+    [
+        # Content for the Save Page Modal Body
+        dcc.Markdown("Write a YAML for running wrmXpress remotely. Include a full path and file name ending in `.yaml`."),
+        dbc.Input(id="file-path-for-saved-yaml-file",
+                  placeholder="Enter the full save path...", type="text"),
+    ]
+)
+
+########################################################################
+####                                                                ####
+####                             Modal                              ####
+####                                                                ####
+########################################################################
+save_page = dbc.Modal(
+    [
+        # Modal content header
+        dbc.ModalHeader("Save Page"),
+        # Modal page content as defined above
+        save_page_content,
+        # Modal page footer
+        dbc.ModalFooter([
+            # Buttons for the Save Page Modal
+            dbc.Button("Save", id="save-page-button", className="ml-auto"),
+            dbc.Button("Close", id="close-save-modal", className="ml-auto"),
+        ]),
+        html.Div(id="save-page-status") 
+    ],
+    id="save-page-modal",
+    size="l"
+)
+
+########################################################################
+####                                                                ####
+####                             Callbacks                          ####
+####                                                                ####
+########################################################################
+def save_page_yaml(app):
+    # Write YAML from save page
+    @app.callback(
+        Output("save-page-status", "children"),
+        [Input("save-page-button", "n_clicks")],
+        [
+            State("imaging-mode", "value"),
+            State("file-structure", "value"),
+            State("multi-well-rows", "value"),
+            State("multi-well-cols", "value"),
+            State("multi-well-detection", "value"),
+            State("species", "value"),
+            State("stages", 'value'),
+            State("motility-run", "value"),
+            State("conversion-run", "value"),
+            State("conversion-scale-video", "value"),
+            State("conversion-rescale-multiplier", "value"),
+            State("segment-run", "value"),
+            State("segmentation-wavelength", 'value'),
+            State("cell-profiler-run", "value"),
+            State("cell-profiler-pipeline", "value"),
+            State("diagnostics-dx", "value"),
+            State("well-selection-list", "children"),
+            State("work-directory", "value"),
+            State("input-directory", "value"),
+            State("output-directory", "value"),
+            State("file-path-for-saved-yaml-file", "value")
+        ]
+    )
+    def save_page_to_yaml(
+        n_clicks,
+        imagingmode,
+        filestructure,
+        multiwellrows,
+        multiwellcols,
+        multiwelldetection,
+        species,
+        stages,
+        motilityrun,
+        conversionrun,
+        conversionscalevideo,
+        conversionrescalemultiplier,
+        segmentrun,
+        wavelength,
+        cellprofilerrun,
+        cellprofilerpipeline,
+        diagnosticdx,
+        wellselection,
+        workdirectory,
+        inputdirectory,
+        outputdirectory,
+        filepathforyamlfile
+    ):
+        if n_clicks:
+            well_list = [s.replace(", ", '') for s in wellselection]
+
+            # Formatting YAML file with correct layout
+            user_input_yaml_file = {
+                "imaging_mode": [imagingmode],
+                "file_structure": [filestructure],
+                "multi-well-rows": multiwellrows,
+                "multi-well-cols": multiwellcols,
+                "multi-well-detection": [multiwelldetection],
+                "species": [species],
+                "stages": [stages],
+                "modules": {
+                    "motility": {"run": bool(motilityrun)},
+                    "convert": {
+                        "run": bool(conversionrun),
+                        "save_video": bool(conversionscalevideo),
+                        "rescale_multiplier": conversionrescalemultiplier
+                    },
+                    "segment": {
+                        "run": bool(segmentrun),
+                        "wavelength": [wavelength]
+                    },
+                    "cellprofiler": {
+                        "run": bool(cellprofilerrun),
+                        "pipeline": [cellprofilerpipeline]
+                    },
+                    "dx": {
+                        "run": bool(diagnosticdx)
+                    }
+                },
+                "wells": well_list,
+                "directories": {
+                    "work": [workdirectory],
+                    "input": [inputdirectory],
+                    "output": [outputdirectory]
+                }
+            }
+            # Create the full filepath using os.path.join
+            output_file = os.path.join(filepathforyamlfile)
+
+            # Dump preview data to YAML file
+            with open(output_file, 'w') as yaml_file:
+                yaml.dump(user_input_yaml_file, yaml_file,
+                        default_flow_style=False)
+            return f"Data Saved to {filepathforyamlfile}"
+        return ""
+
