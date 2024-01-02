08eb532707acce77adc2756b18c6f884a6a6431e
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-02 14:42:55 -0600

    update save page callback

--- app/pages/save_page_content.py
+++ app/pages/save_page_content.py
@@ -7,7 +7,8 @@ import dash_bootstrap_components as dbc
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import yaml
-import os
+from pathlib import Path
+from app.utils.callback_functions import prep_yaml
 
 ########################################################################
 ####                                                                ####
@@ -55,100 +56,63 @@ def save_page_yaml(app):
     # Write YAML from save page
     @app.callback(
         Output("save-page-status", "children"),
-        [Input("save-page-button", "n_clicks")],
-        [
-            State("imaging-mode", "value"),
-            State("file-structure", "value"),
-            State("multi-well-rows", "value"),
-            State("multi-well-cols", "value"),
-            State("multi-well-detection", "value"),
-            State("species", "value"),
-            State("stages", 'value'),
-            State("motility-run", "value"),
-            State("conversion-run", "value"),
-            State("conversion-scale-video", "value"),
-            State("conversion-rescale-multiplier", "value"),
-            State("segment-run", "value"),
-            State("segmentation-wavelength", 'value'),
-            State("cell-profiler-run", "value"),
-            State("cell-profiler-pipeline", "value"),
-            State("diagnostics-dx", "value"),
-            State("well-selection-list", "children"),
-            State("work-directory", "value"),
-            State("input-directory", "value"),
-            State("output-directory", "value"),
-            State("file-path-for-saved-yaml-file", "value")
-        ]
+        Input("save-page-button", "n_clicks"),
+        State('imaging-mode', 'value'),
+        State('file-structure', 'value'),
+        State('multi-well-rows', 'value'),
+        State('multi-well-cols', 'value'),
+        State('multi-well-detection', 'value'),
+        State('species', 'value'),
+        State('stages', 'value'),
+        State('motility-run', 'value'),
+        State('conversion-run', 'value'),
+        State('conversion-scale-video', 'value'),
+        State('conversion-rescale-multiplier', 'value'),
+        State('segment-run', 'value'),
+        State('segmentation-wavelength', 'value'),
+        State('cell-profiler-run', 'value'),
+        State('cell-profiler-pipeline', 'value'),
+        State('diagnostics-dx', 'value'),
+        State('plate-name', 'value'),
+        State('mounted-volume', 'value'),
+        State('well-selection-list', 'children'),
+        prevent_initial_call=True
     )
     def save_page_to_yaml(
-        n_clicks,
-        imagingmode,
-        filestructure,
-        multiwellrows,
-        multiwellcols,
-        multiwelldetection,
-        species,
-        stages,
-        motilityrun,
-        conversionrun,
-        conversionscalevideo,
-        conversionrescalemultiplier,
-        segmentrun,
-        wavelength,
-        cellprofilerrun,
-        cellprofilerpipeline,
-        diagnosticdx,
-        wellselection,
-        workdirectory,
-        inputdirectory,
-        outputdirectory,
-        filepathforyamlfile
-    ):
-        if n_clicks:
-            well_list = [s.replace(", ", '') for s in wellselection]
+        nclicks, imagingmode, filestructure, multiwellrows, multiwellcols, multiwelldetection, species, stages, motilityrun, conversionrun, conversionscalevideo, conversionrescalemultiplier, segmentrun, wavelength, cellprofilerrun, cellprofilerpipeline, diagnosticdx, platename, volume, wells):
+        if nclicks:
+            
+            if wells == 'All':
+                first_well = 'A01'
+            else:
+                first_well = wells[0]
 
-            # Formatting YAML file with correct layout
-            user_input_yaml_file = {
-                "imaging_mode": [imagingmode],
-                "file_structure": [filestructure],
-                "multi-well-rows": multiwellrows,
-                "multi-well-cols": multiwellcols,
-                "multi-well-detection": [multiwelldetection],
-                "species": [species],
-                "stages": [stages],
-                "modules": {
-                    "motility": {"run": bool(motilityrun)},
-                    "convert": {
-                        "run": bool(conversionrun),
-                        "save_video": bool(conversionscalevideo),
-                        "rescale_multiplier": conversionrescalemultiplier
-                    },
-                    "segment": {
-                        "run": bool(segmentrun),
-                        "wavelength": [wavelength]
-                    },
-                    "cellprofiler": {
-                        "run": bool(cellprofilerrun),
-                        "pipeline": [cellprofilerpipeline]
-                    },
-                    "dx": {
-                        "run": bool(diagnosticdx)
-                    }
-                },
-                "wells": well_list,
-                "directories": {
-                    "work": [workdirectory],
-                    "input": [inputdirectory],
-                    "output": [outputdirectory]
-                }
-            }
-            # Create the full filepath using os.path.join
-            output_file = os.path.join(filepathforyamlfile)
+            config = prep_yaml(
+                imagingmode,
+                filestructure,
+                multiwellrows,
+                multiwellcols,
+                multiwelldetection,
+                species,
+                stages,
+                motilityrun,
+                conversionrun,
+                conversionscalevideo,
+                conversionrescalemultiplier,
+                segmentrun,
+                wavelength,
+                cellprofilerrun,
+                cellprofilerpipeline,
+                diagnosticdx,
+                wells
+            )
+
+           # Create the full filepath using os.path.join
+            output_file = Path(volume, platename + '.yml')
 
             # Dump preview data to YAML file
             with open(output_file, 'w') as yaml_file:
-                yaml.dump(user_input_yaml_file, yaml_file,
+                yaml.dump(config, yaml_file,
                         default_flow_style=False)
-            return f"Data Saved to {filepathforyamlfile}"
         return ""
 
