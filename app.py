908720fa346913c2b990c613d55cf691e584863c
golden-dood <catererzach@yahoo.com>
2024-01-02 20:37:04 -0600

    moved app.callback

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ from app.pages.info_page_content import info_page
 from app.pages.preview_page_content import preview_page, load_first_img
 from app.components.tabs_content import tabs_content
 from app.components.change_page_callback import get_callbacks
-from app.components.metadata_tab import open_metadata_offcanvas, add_metadata_table_checklist
+from app.components.metadata_tab import open_metadata_offcanvas
 from app.components.run_time_settings import update_well_selection_table, populate_list_of_wells
 from app.components.instrument_settings import hidden_multi_row_col_feature
 from app.components.create_metadata_tabs_from_checklist import create_metadata_tables_from_checklist
+from app.components.metadata_table_checklist import add_metadata_table_checklist
 
 app = dash.Dash(__name__, external_stylesheets=[
                 dbc.themes.SPACELAB],
--- app/components/metadata_tab.py
+++ app/components/metadata_tab.py
@@ -144,18 +144,3 @@ def open_metadata_offcanvas(app):
         return is_open
 
 
-def add_metadata_table_checklist(app):
-    @app.callback(
-        Output("checklist-input", "options"),
-        [Input("add-metadata-table-button", "n_clicks")],
-        [State("uneditable-input-box", 'value'),
-         State("checklist-input", "options")]
-    )
-    def update_metadata_checklist(n_clicks, new_table_name, existing_options):
-        if n_clicks and new_table_name:
-            # Append the new table name to the existing options
-            new_option = {"label": new_table_name, "value": new_table_name}
-            updated_options = existing_options + [new_option]
-            return updated_options
-        else:
-            return existing_options
