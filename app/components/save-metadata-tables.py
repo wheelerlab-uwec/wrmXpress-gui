6fa53311e92a79756d23d2f4ffc22dcf1c3a276f
golden-dood <catererzach@yahoo.com>
2024-01-05 10:34:52 -0600

    attempting to get datatables to save as csv

---
+++ app/components/save-metadata-tables.py
@@ -0,0 +1,54 @@
+########################################################################
+####                                                                ####
+####                             Imports                            ####
+####                                                                ####
+########################################################################
+import pandas as pd
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+from dash import dash_table
+
+########################################################################
+####                                                                ####
+####                           CALLBACKS                            ####
+####                                                                ####
+########################################################################
+def save_metadata_tables_to_csv(app):
+    @app.callback(
+        Output("save-meta-data-to-csv", "disabled"),
+        [Input("save-meta-data-to-csv", "n_clicks")],
+        [State('metadata-tabs', 'children')]
+    )
+    def save_the_metadata_tables_to_csv(n_clicks, metadata_tabs):
+        if n_clicks:
+            # Iterate over the metadata tabs
+            for tab in metadata_tabs:
+                tab_id = tab['props']['value']  # Get the tab ID
+                table_id = f'{tab_id}-tab-table'  # Construct the table ID
+
+                # Find the correct table object inside the tab
+                table_obj = None
+                for child in tab['props']['children']:
+                    if child['id'] == table_id:
+                        table_obj = child
+                        break
+
+                if table_obj:
+                    table_data = table_obj['props']['children']['props']['data']  # Get the data from the table
+
+                    # Convert the table data to a DataFrame
+                    df = pd.DataFrame(table_data)
+
+                    # Replace empty cells with "None"
+                    df = df.where(pd.notnull(df), "None")
+
+                    # Save the DataFrame to a CSV file
+                    file_path = f"/Users/zach/avacado_analytics/wrmXpress_github/wrmXpress-gui/practice_output_folder/{tab_id}.csv"
+                    df.to_csv(file_path, index=False)
+
+            # Re-enable the button after saving
+            return False
+
+        # Enable the button if not clicked
+        return False
