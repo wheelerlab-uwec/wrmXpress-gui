b98f7f3f81d955687c9589985b66b756d60606a3
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-11 11:16:03 -0600

    add more reqs

--- app/components/test_scripts/worm_informtaion_test.py
+++ app/components/test_scripts/worm_informtaion_test.py
@@ -28,37 +28,38 @@ from app.components.save_metadata_tables import save_metadata_tables_to_csv
 ####                           Testing                              ####
 ####                                                                ####
 ########################################################################
-def test_002_worm_information(dash_duo):
-    #defining the app
-    app = dash.Dash(__name__, external_stylesheets=[
-                dbc.themes.SPACELAB], 
-                suppress_callback_exceptions=True)
-    app.layout = html.Div([header, 
-                       # Adding vertical space so tabs content not hidden behind navbar
-                       html.H4("",style={'padding-top': 80, 'display': 'inline-block'}),
-                       tabs_content, 
-                       # Modals (popup screens)
-                        save_page,
-                        info_page,
-                        preview_page,
-                       ])
 
-    #hosting the app
+
+def test_002_worm_information(dash_duo):
+    # defining the app
+    app = dash.Dash(__name__, external_stylesheets=[
+        dbc.themes.SPACELAB],
+        suppress_callback_exceptions=True)
+    app.layout = html.Div([header,
+                           # Adding vertical space so tabs content not hidden behind navbar
+                           html.H4(
+                               "", style={'padding-top': 80, 'display': 'inline-block'}),
+                           tabs_content,
+                           # Modals (popup screens)
+                           save_page,
+                           info_page,
+                           preview_page,
+                           ])
+
+    # hosting the app
     dash_duo.start_server(app)
 
-    #testing for imaging-mode 
-    #id_list contains all known id's in the code
+    # testing for imaging-mode
+    # id_list contains all known id's in the code
     id_list = ['species', 'stages', 'worm-information', '_dbcprivate_radioitems_species_input_Bma', '_dbcprivate_radioitems_species_input_Cel',
                '_dbcprivate_radioitems_species_input_Sma', '_dbcprivate_radioitems_stages_input_Mf', '_dbcprivate_radioitems_stages_input_Adult',
                '_dbcprivate_radioitems_stages_input_Mixed']
-    
-    #tests for the existacne of specfic elements within the html 
+
+    # tests for the existacne of specfic elements within the html
     for i in id_list:
         s1 = '#'
         new_string = s1+i
         wait = WebDriverWait(dash_duo._driver, 10)
-        element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, new_string)))
+        element = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, new_string)))
         assert element is not None, f"Element with id of '{i}' not found"
-
-        
-
