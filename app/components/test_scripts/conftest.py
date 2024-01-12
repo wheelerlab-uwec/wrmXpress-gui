7535b3fe4523e2091d1ca5095380f62f112909c3
mathisensage <mathisensage@gmail.com>
2024-01-12 10:22:27 -0600

    re-enabled headless mode

--- app/components/test_scripts/conftest.py
+++ app/components/test_scripts/conftest.py
@@ -2,6 +2,6 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_setup_options():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     return options
