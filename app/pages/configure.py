e10b4e631f9753854b37da41a7b1378481e8e5e8
golden-dood <catererzach@yahoo.com>
2024-01-15 09:56:48 -0600

    updated error message with proper formatting

--- app/pages/configure.py
+++ app/pages/configure.py
@@ -46,7 +46,7 @@ layout = dbc.Container([
         always_open=True,
     ),
     html.Hr(),
-    dbc.Alert(id='resolving-error-issue-configure',is_open=False, color='success', duration = 6000),
+    dbc.Alert(id='resolving-error-issue-configure',is_open=False, color='danger', duration = 6000),
     html.Br(),
     dbc.Row(
         [
@@ -216,28 +216,33 @@ def run_analysis(
     if nclicks:
 
         try:
-
+            error_messages = ["While finalizing the configuration, the following errors were found:"]
+            error_occured = False
             """
             Checking volume and plate names to ensure they are adequately named
             """
             check_cases = [None, '', '/']
             if platename in check_cases: 
-                return 'success', True, "Platename is missing; please add a Platename."
+                error_occured = True
+                error_messages.append("Platename is missing; please add a Platename. ")
             if volume in check_cases:
-                return 'success', True, "Volume is missing; please add a Volume path."
+                error_occured = True
+                error_messages.append("Volume is missing; please add a Volume path. ")
 
             platename_parts = list(platename)
             if len(platename_parts) > 0:
                 platename_parts_start = platename_parts[0]
                 platename_parts_end = platename_parts[-1]
                 if platename_parts_start in check_cases or platename_parts_end in check_cases:
-                    return 'success', True,  'Platename is inadequatly named; please add an adequate Platename'
+                    error_occured = True
+                    error_messages.append('Platename is inadequatly named; please add an adequate Platename. ')
             
             volume_parts=list(volume)
             if len(volume_parts)>0:
                 volume_parts = volume_parts[-1]
                 if volume_parts in check_cases:
-                    return "success", True, 'Volume is inadequatly named; please add an adequate Volume'
+                    error_occured = True
+                    error_messages.append('Volume is inadequatly named; please add an adequate Volume. ')
                 
             """
             Checking to see if volume, plate, and input directories exist
@@ -245,11 +250,14 @@ def run_analysis(
             input_path = Path(volume, 'input')
             platename_path = Path(volume, "input", platename)
             if not os.path.exists(volume):
-                return 'success', True, 'Volume is invalid; please choose a valid Volume'
+                error_occured = True
+                error_messages.append('Volume is invalid; please choose a valid Volume. ')
             if not os.path.exists(platename_path):
-                return 'success', True, 'Platename is invalid; please choose a valid Platename'
+                error_occured = True
+                error_messages.append('Platename is invalid; please choose a valid Platename. ')
             if not os.path.exists(input_path):
-                return 'success', True, "No 'input' in the Volume directory; please ensure Volume contains 'input'"
+                error_occured = True
+                error_messages("No 'input' in the Volume directory; please ensure Volume contains 'input'. ")
             
             '''
             Checking to see if the wells selected exist
@@ -258,7 +266,8 @@ def run_analysis(
             for well in wells:
                 img_path = Path(volume, 'input', f'{platename}/TimePoint_1/{plate_base}_{well}.TIF')
                 if not os.path.exists(img_path):
-                    return 'success', True, 'Please ensure you select onley the wells which you wish to analyze'
+                    error_occured = True
+                    error_messages.append('You have selected more wells than you have images. Please ensure you select onley the wells which you wish to analyze. ')
 
             """
             Checking if video module is selected with only one time point
@@ -267,17 +276,27 @@ def run_analysis(
                 for i in range(2,3):
                     timept = Path(volume, 'input', f'{platename}/TimePoint_{i}')
                     if not os.path.exists(timept):
-                        return 'success', True, 'Please ensure accurate selections of the configuration page'
+                        error_occured = True
+                        error_messages.append('You have selected cell profiler while having multiple time points, please ensure accurate selections. ')
 
             """
             Checking for conflicting modules
             """
             if eval_bool(cellprofilerrun) ==True and eval_bool(segmentrun):
-                return 'success', True, 'please ensure accurate selections'
+                error_occured = True
+                error_messages.append('Cannot run Cellprofiler and Segment together.')
   
             if eval_bool(cellprofilerrun) ==True and eval_bool(motilityrun):
-                return 'success', True, 'please ensure accurate selections'
-  
+                error_occured = True
+                error_messages.append('Cannot run Cellprofiler and Motility together.')
+
+            if error_occured == True:
+                error_messages[0] = html.H4(f'{error_messages[0]}', className='alert-heading')
+                # Format the error messages
+                for i in range(1,len(error_messages)):
+                    error_messages[i] = html.P(f'{i}. {error_messages[i]}', className="mb-0")
+
+                return 'success', True, error_messages
 
         except ValueError:
             return 'success', True, 'A ValueError occurred'
