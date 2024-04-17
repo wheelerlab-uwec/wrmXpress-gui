cf5c389d2171a48eb46ea8594ad908c54509a9aa
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-04-17 11:59:16 -0500

    started changing info

--- app/components/info_layout.py
+++ app/components/info_layout.py
@@ -16,70 +16,76 @@ from dash import dcc, html
 info_layout = [
     html.H3(
         # Header of Information & Usage
-        "Information & Usage"
+        "Information"
     ),
 
     # Introduction to wrmXpress
     html.P(
-        "wrmXpress is a unified framework for the analysis of diverse phenotypic imaging data in high-throughput and high-content experiments involving free-living and parasitic nematodes and flatworms. wrmXpress is a versatile solution capable of handling large datasets and generating relevant phenotypic measurements across various worm species and experimental assays. This software, designed to analyze a spectrum of phenotypes such as motility, development/size, fecundity, and feeding, not only addresses current research needs but also establishes itself as a platform for future extensibility, enabling the development of custom phenotypic modules."
+        "wrmXpress is a unified framework for the analysis of diverse phenotypic imaging data in "
+        "high-throughput and high-content experiments involving free-living and parasitic nematodes "
+        "and flatworms. This GUI is designed to be a user-friendly option for running smaller-scale "
+        "analyses. For large scale high-throughput experiments, we recommend running wrmXpress on a "
+        "high-performance or high-throughput computer cluster."
     ),
 
     # Header of Usage
     html.H3(
-        "Usage"
+        "GUI Usage"
     ),
-    html.P(
+    dcc.Markdown(
         # Content of usage
-        "There are two options for running wrmXpress:"
+        "To configure and run analyses, use the navigation column at left-hand side of the screen. \n"
+        "**Configure** will allow you to select relevant experimental and imaging parameters, as "
+        "well as select the pipeline that you wish to run."
     ),
 
     # Content of usage seperated into cards
-    dbc.Row(
-        [
-            dbc.Col([
-                    # Remote card
-                    dbc.Card([
-                        dbc.CardBody(
-                            [
-                                # Remote card
-                                html.H5(
-                                    "Remotely",
-                                    className="card-title" # Defines the style of the card title
-                                ),
-                                dcc.Markdown(
+    # dbc.Row(
+    #     [
+    #         dbc.Col([
+    #                 # Remote card
+    #                 dbc.Card([
+    #                     dbc.CardBody(
+    #                         [
+    #                             # Remote card
+    #                             html.H5(
+    #                                 "Remotely",
+    #                                 className="card-title" # Defines the style of the card title
+    #                             ),
+    #                             dcc.Markdown(
 
-                                    # Remote card content
-                                    "wrmXpress was originally designed to be invoked remotely, using a high-performance or high-throughput computer. There are many ways to go about this, but we recommend encapsulating the entire process in a Docker container. The container should include the Python libraries required by wrmXpress (see the [Zamanian Lab's Dockerfile/conda environment](https://github.com/zamanianlab/Docker/tree/main/chtc-wrmxpress) for an example), the cloned [wrmXpress repository](https://github.com/zamanianlab/wrmXpress), the input data in a directory called `input/`, and a YAML file that configures the analysis. A user can use this graphical user interface (GUI) to produce the YAML by selecting the options and modules and clicking Save YAML.",
-                                    className="card-text" # Defines the style of the card text
-                                )
-                            ]
-                        )
-                    ],
-                        color='light'  # Defines the color for the card
-                    )
-                    ]),
-            dbc.Col([
-                    # Locally card
-                    dbc.Card([
-                        dbc.CardBody(
-                             [
-                                 # Locally Card
-                                 html.H5(
-                                     "Locally", 
-                                     className="card-title" # Defines the style of the card title
-                                 ),
-                                 # Local card content
-                                 dcc.Markdown(
-                                     "Many analyses, such as those that include a few dozen separate videos or images, can be performed on a desktop computer without the need for high-performance or high-throughput computing. For these, a user can use this GUI to select the options and modules and run the analysis by clicking the Preview and Run button.",
-                                     className="card-text" # Defines the style of the card text
-                                 )
-                             ]
-                             )
-                    ],
-                        color='light'  # Defines the color for the card
-                    )
-                    ])
-        ]),
+    #                                 # Remote card content
+    #                                 "wrmXpress was originally designed to be invoked remotely, using a high-performance or high-throughput computer. There are many ways to go about this, but we recommend encapsulating the entire process in a Docker container. The container should include the Python libraries required by wrmXpress (see the [Zamanian Lab's Dockerfile/conda environment](https://github.com/zamanianlab/Docker/tree/main/chtc-wrmxpress) for an example), the cloned [wrmXpress repository](https://github.com/zamanianlab/wrmXpress), the input data in a directory called `input/`, and a YAML file that configures the analysis. A user can use this graphical user interface (GUI) to produce the YAML by selecting the options and modules and clicking Save YAML.",
+    #                                 className="card-text" # Defines the style of the card text
+    #                             )
+    #                         ]
+    #                     )
+    #                 ],
+    #                     color='light'  # Defines the color for the card
+    #                 )
+    #                 ]),
+    #         dbc.Col([
+    #                 # Locally card
+    #                 dbc.Card([
+    #                     dbc.CardBody(
+    #                          [
+    #                              # Locally Card
+    #                              html.H5(
+    #                                  "Locally", 
+    #                                  className="card-title" # Defines the style of the card title
+    #                              ),
+    #                              # Local card content
+    #                              dcc.Markdown(
+    #                                  "Many analyses, such as those that include a few dozen separate videos or images, can be performed on a desktop computer without the need for high-performance or high-throughput computing. For these, a user can use this GUI to select the options and modules and run the analysis by clicking the Preview and Run button.",
+    #                                  className="card-text" # Defines the style of the card text
+    #                              )
+    #                          ]
+    #                          )
+    #                 ],
+    #                     color='light'  # Defines the color for the card
+    #                 )
+    #                 ])
+    #     ]),
     html.Br(),
     # Developers section of information page
     html.H3(
