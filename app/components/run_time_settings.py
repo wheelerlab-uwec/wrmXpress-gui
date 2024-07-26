aa61675dee5e9d16394b3d09962128bea8c87209
Zachary Caterer <catererzach@yahoo.com>
2024-07-26 13:02:10 -0500

    added info symbol and tooltip elements to volume path and plate name

--- app/components/run_time_settings.py
+++ app/components/run_time_settings.py
@@ -23,37 +23,46 @@ run_time_settings = dbc.AccordionItem(
                     [
                         # Title for Imaging mode
                         html.H5("Directories", id="directories-mode-header"),
-                        dbc.Col(
+                        dbc.Tooltip(
                             [
-                                dbc.Tooltip(
-                                    [
-                                        # Tooltip element for information symbol, displays message when cursor over the symbol
-                                        html.P(
-                                            "Running in Docker: use `/home/`.",
-                                            style={"text-align": "left"},
-                                        ),
-                                        html.P(
-                                            "Running natively: use the path to the parent directory that contains a directory of images.",
-                                            style={"text-align": "left"},
-                                        ),
-                                    ],
-                                    placement="left",
-                                    target="volume-path-label",
+                                # Tooltip element for information symbol, displays message when cursor over the symbol
+                                html.P(
+                                    "Running in Docker: use `/home/`.",
+                                    style={"text-align": "left"},
                                 ),
-                            ]
+                                html.P(
+                                    "Running natively: use the path to the parent directory that contains a directory of images.",
+                                    style={"text-align": "left"},
+                                ),
+                            ],
+                            placement="left",
+                            target="mounted-info-icon",
                         ),
-                        dbc.Col(
+                        html.H6("Volume Path:", id="volume-path-label"),
+                        html.Div(
                             [
-                                html.H6("Volume Path:", id="volume-path-label"),
+                                html.I(
+                                    className="fa-solid fa-circle-info",  # Information symbol
+                                    id="mounted-info-icon",
+                                    style={
+                                        "margin-right": "20px"
+                                    },  # Add some space between icon and input
+                                ),
                                 dbc.Input(  # Input field for the mounted volume path
                                     id="mounted-volume",
                                     placeholder="Path",
                                     type="text",
                                     persistence=True,
                                     persistence_type="memory",
+                                    style={
+                                        "flex": "2"
+                                    },  # Make the input take remaining space
                                 ),
                             ],
-                            width="middle",
+                            style={
+                                "display": "flex",
+                                "align-items": "center",
+                            },
                         ),
                     ],
                     align="center",
@@ -61,13 +70,45 @@ run_time_settings = dbc.AccordionItem(
             ]
         ),
         html.Br(),  # Line break for spacing
-        html.H6("Plate/Folder name:"),  # Label for the plate/folder name input field
-        dbc.Input(  # Input field for the plate/folder name
-            id="plate-name",
-            placeholder="Name",
-            type="text",
-            persistence=True,
-            persistence_type="memory",
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        html.H6(
+                            "Plate/Folder name:"
+                        ),  # Label for the plate/folder name input field
+                        html.Div(
+                            [
+                                dbc.Tooltip(
+                                    html.P(
+                                        "Name of the folder that contains imaging data structured as shown in Info.",
+                                        style={"text-align": "left"},
+                                    ),
+                                    placement="left",
+                                    target="plate-info-icon",
+                                ),
+                                html.I(
+                                    className="fa-solid fa-circle-info",
+                                    id="plate-info-icon",
+                                    style={"margin-right": "20px"},
+                                ),
+                                dbc.Input(  # Input field for the plate/folder name
+                                    id="plate-name",
+                                    placeholder="Name",
+                                    type="text",
+                                    persistence=True,
+                                    persistence_type="memory",
+                                    style={"flex": "2"},
+                                ),
+                            ],
+                            style={
+                                "display": "flex",
+                                "align-items": "center",
+                            },
+                        ),
+                    ]
+                ),
+            ]
         ),
         html.Br(),  # Line break for spacing
         html.Div(
